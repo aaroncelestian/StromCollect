1d6747c259733cdd7dcd609011c9b5c1e52ba315
Aaron Celestian <aaron.celestian@gmail.com>
2025-09-05 21:01:13 -0700

    Initial Commit

---
+++ StromCollectTests/StromCollectTests.swift
@@ -0,0 +1,17 @@
+//
+//  StromCollectTests.swift
+//  StromCollectTests
+//
+//  Created by Aaron Celestian on 9/5/25.
+//
+
+import Testing
+@testable import StromCollect
+
+struct StromCollectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
